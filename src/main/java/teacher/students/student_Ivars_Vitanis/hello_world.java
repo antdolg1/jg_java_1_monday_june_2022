f8f53999605dc93fac0a19c48a7b9b4e0e1a9d23
Amsteramster199 <amsteramster199@gmail.com>
2022-06-20 22:02:59 +0300

    hello_world.java

---
+++ src/main/java/teacher/students/student_Ivars_Vitanis/hello_world.java
@@ -0,0 +1,9 @@
+package main.java.teacher.students.student_Ivars_Vitanis;
+
+public class hello_world {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
