d0a09ac5380fef47a9ffb22468e581bdd46313ce
Supesuba <asura122@gmail.com>
2022-06-20 23:33:00 +0300

    commit Homework: Task 11,12.

--- src/main/java/teacher/students/student_Vlad_Pochepko/lesson_1/HelloWorld.java
+++ src/main/java/teacher/students/student_Vlad_Pochepko/lesson_1/HelloWorld.java
@@ -1,4 +1,4 @@
-package main.java.teacher.students.student_Vlad_Pochepko;
+package main.java.teacher.students.student_Vlad_Pochepko.lesson_1;
 
 public class HelloWorld {
     public static void main(String[] args) {
