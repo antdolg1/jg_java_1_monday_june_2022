31870ebc09a07d81b6fa3bd3b5cc820d1dce9e48
Nolife777 <edikstrungevich@gmail.com>
2022-06-20 22:01:18 +0300

    Add HelloStudents program

---
+++ src/main/java/teacher/students/student_edik_gerasimchuk/Test.java
@@ -0,0 +1,10 @@
+package main.java.teacher.students.student_edik_gerasimchuk;
+
+public class Test {
+
+    public static void main(final String[] args) {
+
+        System.out.println("Hello students!");
+    }
+
+}
