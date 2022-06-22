43d3f6e509902b79a3cfcfecc731e860a90f0e37
roberts-pavlovs <pavlovsroberts@inbox.lv>
2022-06-22 21:59:58 +0300

    Add my first program

---
+++ src/main/java/teacher/students/student_robert_pavlov/HelloWorld.java
@@ -0,0 +1,9 @@
+package main.java.teacher.students.student_robert_pavlov;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.println("Hello World!!!");
+    }
+}
