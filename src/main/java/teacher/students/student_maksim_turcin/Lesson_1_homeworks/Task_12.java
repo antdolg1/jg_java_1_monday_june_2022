412fcf2bf85607c4779b8c70ad9e550d339d0c9e
M4488 <maksims.turcins@gmail.com>
2022-06-23 08:12:36 +0300

    *

---
+++ src/main/java/teacher/students/student_maksim_turcin/Lesson_1_homeworks/Task_12.java
@@ -0,0 +1,18 @@
+package main.java.teacher.students.student_maksim_turcin.Lesson_1_homeworks;
+
+public class Task_12 {
+
+  public static void main(String[] args){
+      int a = 10;
+      int b = 11;
+
+      System.out.println( a * b);
+
+
+
+  }
+
+
+
+
+}
