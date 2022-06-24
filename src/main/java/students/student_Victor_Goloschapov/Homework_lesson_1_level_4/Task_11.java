e58cfad8085a57987ed1bbfb0c3ded13b5e16d51
VictorGoloschapov <victor.goloschapov@gmail.com>
2022-06-24 15:45:51 +0300

    add Task_11 app

---
+++ src/main/java/students/student_Victor_Goloschapov/Homework_lesson_1_level_4/Task_11.java
@@ -0,0 +1,19 @@
+package main.java.students.student_Victor_Goloschapov.Homework_lesson_1_level_4;
+
+public class Task_11 {
+    public static void main(String[] args) {
+        //sum int
+        int a  = 74,
+            b = 36,
+            c = a + b;
+
+        System.out.println("Result is: " + c);
+
+        //divide int
+        int d = 50,
+            e = 3,
+            f = d / 3;
+
+        System.out.println("Result is: " + f);
+    }
+}
