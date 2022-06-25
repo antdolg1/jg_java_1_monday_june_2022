d202f6e0e3274a0dcbdf6cf11d7b41076a13c7d0
OlgaNikolPavlova <onnnn96@gmail.com>
2022-06-25 16:07:01 +0300

    hello

---
+++ src/main/java/students/student_olga_novoselova/Devide_of_two_digits.java
@@ -0,0 +1,10 @@
+package main.java.students.student_olga_novoselova;
+
+public class Devide_of_two_digits {
+    public static void main(String[] args) {
+        int x = 50;
+        int y = 3;
+        int dev = x/y;
+        System.out.println(dev);
+    }
+}
