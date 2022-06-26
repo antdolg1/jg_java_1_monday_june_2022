ad0a1cc906124afb13c35f638f4aae383356f5a6
Antons Dolgopolovs <antdolg@gmail.com>
2022-06-26 19:48:11 +0300

    Add comments regarding homework

--- src/main/java/students/student_Victor_Goloschapov/Homework_lesson_1_level_4/Task_11.java
+++ src/main/java/students/student_Victor_Goloschapov/Homework_lesson_1_level_4/Task_11.java
@@ -1,18 +1,21 @@
 package main.java.students.student_Victor_Goloschapov.Homework_lesson_1_level_4;
 
 public class Task_11 {
-    public static void main(String[] args) {
+    //Название класса не соответствует Java конвенции.
+    //В названиях класса мы не используем snake_case, а используем PascalCase.
+    public static void main(final String[] args) {
         //sum int
-        int a  = 74,
-            b = 36,
-            c = a + b;
+        final int a = 74;
+        final int b = 36;
+        final int c = a + b;
 
         System.out.println("Result is: " + c);
 
         //divide int
-        int d = 50,
-            e = 3,
-            f = d / 3;
+        final int d = 50;
+        final int e = 3;
+        final int f = d / 3;
+        //Для чего создавать и инициализировать переменную если вы её потом не используете?
 
         System.out.println("Result is: " + f);
     }
