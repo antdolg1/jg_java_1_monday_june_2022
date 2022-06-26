e7ddd6f2e09c41ee22075eb53124c27c4ed22afd
Aljenka <aljenka_@inbox.lv>
2022-06-26 19:47:10 +0300

    Add Task_12

---
+++ src/main/java/students/student_aljona_rekete/lesson_1/level_4/Task_12.java
@@ -0,0 +1,13 @@
+package main.java.students.student_aljona_rekete.lesson_1.level_4;
+
+public class Task_12 {
+
+    public static void main(final String[] args)  {
+
+        int a = 50;
+        int b = 3;
+
+        System.out.println(a*b);
+
+    }
+}
