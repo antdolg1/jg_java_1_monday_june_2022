eb0fd9a9ed8e4ce7066fd2703f4b7dd03621c9fb
M4488 <maksims.turcins@gmail.com>
2022-06-26 09:24:45 +0300

    12 NEW 123 4+ test.txt

--- src/main/java/students/student_maksim_turcin/Lesson_1_homeworks/Task_11.java
+++ src/main/java/students/student_maksim_turcin/Lesson_1_homeworks/Task_11.java
@@ -2,19 +2,12 @@ package main.java.students.student_maksim_turcin.Lesson_1_homeworks;
 
 public class Task_11 {
     public static void main(final String[] args)  {
-
          final int a = 51;
          final int b = 49;
-
          System.out.println( a+b);
-
          System.out.println();
-
           final int p = 55;
           final int y = 17;
-
-
-
 
 
 
