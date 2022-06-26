78c9e30ab75087b21535e887e1f377453bdf20eb
Amsteramster199 <amsteramster199@gmail.com>
2022-06-26 20:18:07 +0300

    Task_11(upd)

---
+++ src/main/java/students/student_Ivars_Vitanis/Task_11_1.java
@@ -0,0 +1,21 @@
+package main.java.students.student_Ivars_Vitanis;
+
+public class Task_11_1 {
+    public static void main(String[] args) {
+
+        int a = 55;
+        int b = 1;
+
+        System.out.println(a + b);
+
+
+        System.out.println();
+
+        int c = 12;
+        int g = 6;
+
+        System.out.println(c / g);
+
+    }
+}
+
