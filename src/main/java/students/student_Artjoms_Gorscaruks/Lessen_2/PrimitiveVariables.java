5cb7caf6fa2910b1e19648194151d4efd9e5d398
agvagv123 <kinja1297@gmail.com>
2022-06-27 21:42:52 +0300

    Homework

---
+++ src/main/java/students/student_Artjoms_Gorscaruks/Lessen_2/PrimitiveVariables.java
@@ -0,0 +1,34 @@
+package main.java.students.student_Artjoms_Gorscaruks.Lessen_2;
+
+public class PrimitiveVariables {
+    public static void main(String[] args) {
+
+        byte myByte = 5;
+        System.out.println("myByte result is:" + myByte);
+
+        short myShort = 100;
+        System.out.println("myShort resualt is: " + myShort);
+
+        int myInt = 32212;
+        System.out.println("myInt result is: " + myInt);
+
+        long myLong = 1000000;
+        System.out.println("myLong result is: " + myLong);
+
+        float myFloat = 2.15f;
+        System.out.println("myFloat result is: " + myFloat);
+
+        double myDouble = 3.542342;
+        System.out.println("myDouble result is: " + myDouble);
+
+        boolean myBoolean = true;
+        System.out.println("myBoolean result is: " + myBoolean);
+
+        char myChar = 'b';
+        System.out.println("myChar result is: " + myChar);
+
+    }
+
+
+
+}
