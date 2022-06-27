3083c404cf8b397bbf140506c845c448d361005c
Amsteramster199 <amsteramster199@gmail.com>
2022-06-27 21:55:31 +0300

    primitiveVariables

---
+++ src/main/java/students/student_Ivars_Vitanis/_Lesson_2/PrimitiveVariables.java
@@ -0,0 +1,31 @@
+package main.java.students.student_Ivars_Vitanis._Lesson_2;
+
+public class PrimitiveVariables {
+    public static void main(String[] args) {
+        byte mybyte = 17;
+        System.out.println("mybyte result is:" + mybyte);
+
+        short myShort = 977;
+        System.out.println("short result is:" + myShort);
+
+        int myInt = 3300;
+        System.out.println("int result is:" + myInt);
+
+        long myLong = 77777;
+        System.out.println("long result is:" + myLong);
+
+        float myFloat = 3.14f;
+        System.out.println("float result is:" + myFloat);
+
+        double myDouble = 52.300;
+        System.out.println("double result is:" + myDouble);
+
+        boolean myBoolean = true;
+        System.out.println("boolean result is:" + myBoolean);
+
+        char myChar = 'b';
+        System.out.println("char result is:" + myChar);
+
+
+    }
+}
