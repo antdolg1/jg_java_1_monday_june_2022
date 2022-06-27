4b0d44fab3bc569671f801ecb2e9657d7c3f55d2
agvagv123 <kinja1297@gmail.com>
2022-06-27 20:43:07 +0300

    Add Hello world

---
+++ src/main/java/students/student_Artjoms_Gorscaruks/Lesson1_homework/Task_11.java
@@ -0,0 +1,23 @@
+package main.java.students.student_Artjoms_Gorscaruks.Lesson1_homework;
+
+public class Task_11 {
+    public static void main(String [] args) {
+
+        int a = 3;
+        int b = 8;
+
+        System.out.println(a + b);
+
+
+
+    int c = 60;
+    int d = 15;
+
+    System.out.println(c / d);
+
+
+
+    }
+
+
+}
---
+++ src/main/java/students/student_Artjoms_Gorscaruks/Lesson1_homework/Task_12.java
@@ -0,0 +1,12 @@
+package main.java.students.student_Artjoms_Gorscaruks.Lesson1_homework;
+
+public class Task_12 {
+    public static void main(String [] args){
+
+        int a = 13;
+        int b = 6;
+        System.out.println(a * b);
+        
+
+    }
+}
