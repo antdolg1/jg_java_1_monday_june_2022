8a9828b25cb0ce863305bed1c2c80a28418d2bb4
Supesuba <asura122@gmail.com>
2022-06-27 22:26:00 +0300

    fixed class name to PascalCase, variables name to camelCase. fixed the name to Java convention standard.

--- src/main/java/students/student_Vlad_Pochepko/lesson_1/HelloWorld.java
+++ src/main/java/students/student_Vlad_Pochepko/lesson_1/HelloWorld.java
@@ -2,7 +2,7 @@ package main.java.students.student_Vlad_Pochepko.lesson_1;
 
 public class HelloWorld {
     public static void main(final String[] args) {
-        System.out.println("Hello World ~");
 
+        System.out.println("Hello World ~");
     }
 }
--- src/main/java/students/student_Vlad_Pochepko/lesson_1/Task11.java
+++ src/main/java/students/student_Vlad_Pochepko/lesson_1/Task11.java
@@ -1,20 +1,25 @@
 package main.java.students.student_Vlad_Pochepko.lesson_1;
 
-public class Task_11 {
+public class Task11 {
     public static void main(final String[] args) {
-        final int num_1 = 74;
-        final int num_2 = 36;
-        final int result = num_1 + num_2;
-        System.out.println(num_1 + " + " + num_2);
-        System.out.println("Result =" + result);
+
+        final int num1 = 74;
+        final int num2 = 36;
+        final int result1 = num1 + num2;
+
+        System.out.println(num1 + " + " + num2);
+
+        System.out.println("Result = " + result1);
 
         System.out.println();
 
-        final int num_3 = 50;
-        final int num_4 = 3;
-        final int result_2 = num_3 / num_4;
-        System.out.println(num_3 + " / " + num_4);
-        System.out.println("Result =" + result_2);
+        final int num3 = 50;
+        final int num4 = 3;
+        final int result2 = num3 / num4;
+
+        System.out.println(num3 + " / " + num4);
+
+        System.out.println("Result = " + result2);
 
         //Название класса и переменных не соответствует Java конвенции.
         //В названиях класса мы не используем snake_case, а используем PascalCase. А в названиях переменных мы используем
