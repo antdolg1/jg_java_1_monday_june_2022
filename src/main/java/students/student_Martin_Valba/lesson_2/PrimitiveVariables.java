943f00a3fa98fb43dd61d3804c29adb051b0b118
pecoyn <martinvalba@mail.ru>
2022-06-27 21:43:01 +0300

    Add class PrimitiveVariables

---
+++ src/main/java/students/student_Martin_Valba/lesson_2/PrimitiveVariables.java
@@ -0,0 +1,47 @@
+package main.java.students.student_Martin_Valba.lesson_2;
+
+public class PrimitiveVariables {
+
+    public static void main(String[] args) {
+
+        byte myByte = 6;
+        System.out.println("myByte result is: " + myByte);
+
+        short myShort = 228;
+        System.out.println("myShort result is: " + myShort);
+
+        int myInt = 580855;
+        System.out.println("myInt result is: " + myInt);
+
+        long myLong = 399143718;
+        System.out.println("myLong result is: " + myLong);
+
+        float myFloat = 6.4123f;
+        System.out.println("myFloat result is: " + myFloat);
+
+        double myDouble = 6.4213;
+        System.out.println("myDouble result is: " + myDouble);
+
+        boolean myBoolean = true;
+        System.out.println("myBoolean result is: " + myBoolean);
+
+        char myChar = 'B';
+        System.out.println("myChar result is: " + myChar);
+
+        int a = 23;
+        int b = 43;
+
+        System.out.println("-= My variables values =-");
+        System.out.println("a variable value: "+a +" some text "+b +a +" "+a +a +b);
+        System.out.println();
+        System.out.println("b variable value: "+b);
+        System.out.println("________________");
+        System.out.println("very " + "hot " + "day ");
+        System.out.println("very hot day");
+
+
+
+    }
+
+}
+
