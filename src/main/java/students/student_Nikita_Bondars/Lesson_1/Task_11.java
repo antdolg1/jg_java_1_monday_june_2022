814080188b2d40870a189b716649dfc29a09522b
NickBRiga <bondars.nikita@gmail.com>
2022-06-27 17:51:19 +0300

    Add 1st homework

---
+++ src/main/java/students/student_Nikita_Bondars/Lesson_1/Task_11.java
@@ -0,0 +1,17 @@
+package main.java.students.student_Nikita_Bondars.Lesson_1;
+
+public class Task_11 {
+    public static void main(final String[] args)  {
+        final int a = 13;
+        final int b = 21;
+
+        System.out.println("Результат:");
+        System.out.println(a + b);
+
+        final int c = 69;
+        final int d = 23;
+
+        System.out.println("Результат:");
+        System.out.println(c / d);
+    }
+}
---
+++ src/main/java/students/student_Nikita_Bondars/Lesson_1/Task_12.java
@@ -0,0 +1,12 @@
+package main.java.students.student_Nikita_Bondars.Lesson_1;
+
+public class Task_12 {
+    public static void main(final String[] args){
+
+        final int a = 13;
+        final int b = 12;
+
+        System.out.println("Результат:");
+        System.out.println(a * b);
+}
+}
