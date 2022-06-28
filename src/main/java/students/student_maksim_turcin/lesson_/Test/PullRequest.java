7df883b0d61b4dc54624374a390ef9993b2ecf5c
M4488 <maksims.turcins@gmail.com>
2022-06-29 01:58:57 +0300

    Commit + int b

--- src/main/java/students/student_maksim_turcin/lesson_/Test/PullRequest.java
+++ src/main/java/students/student_maksim_turcin/lesson_/Test/PullRequest.java
@@ -1,8 +1,9 @@
 package main.java.students.student_maksim_turcin.lesson_.Test;
 
-public class PullR {
+public class PullRequest {
     public static void main(final String[] args) {
 
         int a = 2;
+        int b = 4;
     }
 }
