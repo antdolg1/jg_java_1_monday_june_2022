7ad508ea5bbca21a11df172ae88ee536603481ff
DenissKaira <dkaira1310@gmail.com>
2022-06-29 21:27:20 +0300

    Variables

---
+++ src/main/java/students/student_Denis_Kaira/Lesson_1/Homework/Variables.java
@@ -0,0 +1,33 @@
+package main.java.students.student_Denis_Kaira.Lesson_1.Homework;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        byte myByte = 8;
+        System.out.println("myByte is value is: " + myByte);
+
+        short myShort = 100;
+        System.out.println("myShort is value is: " + myShort);
+
+        int myInt = 32000;
+        System.out.println("myInt is value is: " + myInt);
+
+        long myLong = 1000000;
+        System.out.println("myLong is value is: " + myLong);
+
+        float myFloat = 3.14f;
+        System.out.println("myFloat is value is: " + myFloat);
+
+        double myDouble = 3.54;
+        System.out.println("myDouble is value is: " + myDouble);
+
+        boolean approve = true;
+        System.out.println("approve is value is: " + approve);
+
+        boolean decline = false;
+        System.out.println("decline is value is: " + decline);
+
+        char myChar = 'B';
+        System.out.println("myChar is value is: " + myChar);
+    }
+}
