f6529aa071fd6c879b6c07b1b13bcbde3e9ff0a2
roberts-pavlovs <pavlovsroberts@inbox.lv>
2022-06-29 22:43:18 +0300

    Add my homework

---
+++ src/main/java/teacher/students/student_robert_pavlov/lesson_2/PrimitiveVariables.java
@@ -0,0 +1,31 @@
+package main.java.teacher.students.student_robert_pavlov.lesson_2;
+
+public class PrimitiveVariables {
+    public static void main(String[] args) {
+
+        byte myByte = 8;
+        System.out.println("myByte value is: " + myByte);
+
+        short myShort = 100;
+                System.out.println("myShort value is: " + myShort);
+
+        int myInt = 32000;
+                System.out.println("myInt value is: " + myInt);
+
+        long myLong = 1_000_000;
+                System.out.println("myLong value is: " + myLong);
+
+        float myFloat =  3.145455437f;
+                System.out.println("myFloat value is: " + myFloat);
+
+        double myDouble = 3.145455437;
+                System.out.println("myDouble value is: " + myDouble);
+
+        boolean myBoolean = true;
+                System.out.println("myBoolean value is: " + myBoolean);
+
+        char myChar = 'b';
+                System.out.println("myChar value is: " + myChar);
+
+    }
+}
