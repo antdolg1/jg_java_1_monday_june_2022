3cb4d092d91a680b0729069ec0cb0b7a9e686f7a
roberts-pavlovs <pavlovsroberts@inbox.lv>
2022-06-30 22:53:29 +0300

    Add my homework Task_11_lesson_1

---
+++ src/main/java/students/student_robert_pavlov/Task_11_lesson_1_homework.java
@@ -0,0 +1,13 @@
+package main.java.students.student_robert_pavlov;
+
+public class Task_11_lesson_1_homework {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 4;
+        int result = a * b;
+        System.out.println("Result: " + result);
+        System.out.println(" first number - " + a + " , second number - " + b  );
+
+    }
+}
