52ea6d4235b2185aec97b27958b861d36d1f51a3
roberts-pavlovs <pavlovsroberts@inbox.lv>
2022-06-30 23:17:50 +0300

    Add my homework Task_1_lesson_2

---
+++ src/main/java/students/student_robert_pavlov/lesson_2/Task_1_homework.java
@@ -0,0 +1,26 @@
+package main.java.students.student_robert_pavlov.lesson_2;
+
+public class Task_1_homework {
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 3;
+        int addition = a + b;
+        System.out.println(" result " + addition);
+
+        int c = 2;
+        int d = 2;
+        int multiplication = d * c;
+        System.out.println(" result " + multiplication);
+
+        int e = 2;
+        int f = 2;
+        int subtraction = e - f;
+        System.out.println(" result " + subtraction);
+
+        int g = 2;
+        int h = 2;
+        int division = g / h;
+        System.out.println(" result " + division);
+    }
+}
