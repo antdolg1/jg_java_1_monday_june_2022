91545f76eb51b02baf7d636019932d4b22c86e35
DenissKaira <dkaira1310@gmail.com>
2022-06-30 20:40:35 +0300

    lesson2_task1

---
+++ src/main/java/students/student_Denis_Kaira/Lesson_2/Homework/VivodSKonsoliRezultat.java
@@ -0,0 +1,36 @@
+package main.java.students.student_Denis_Kaira.Lesson_2.Homework;
+
+import java.util.Scanner;
+public class VivodSKonsoliRezultat {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число которое хотите вычесть");
+        int a = in.nextInt();
+        System.out.println("Введите число");
+        int b = in.nextInt();
+        System.out.println("Результат = "+ (a-b));
+
+        System.out.println("Введите число которое хотите сложить");
+        int c = in.nextInt();
+        System.out.println("Введите число");
+        int d = in.nextInt();
+        System.out.println("Результат = "+ (c+d));
+
+        System.out.println("Введите число которое хотите разделить");
+        int f = in.nextInt();
+        System.out.println("Введите число");
+        int g = in.nextInt();
+        System.out.println("Результат = "+ f/g);
+
+        System.out.println("Введите число которое хотите  умножить");
+        int m = in.nextInt();
+        System.out.println("Введите число");
+        int n = in.nextInt();
+        System.out.println("Результат = "+ m*n);
+
+
+    }
+
+}
