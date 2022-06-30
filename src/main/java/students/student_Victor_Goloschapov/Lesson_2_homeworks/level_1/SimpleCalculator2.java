e8a505dd9c75c62ed4cd1de011710876a3808e5e
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-06-30 12:39:28 +0300

    add SimpleCalculator2 class

---
+++ src/main/java/students/student_Victor_Goloschapov/Lesson_2_homeworks/level_1/SimpleCalculator2.java
@@ -0,0 +1,10 @@
+//Task_2
+package Lesson_2_homeworks.level_1;
+import java.util.Scanner;
+
+public class SimpleCalculator2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+}
