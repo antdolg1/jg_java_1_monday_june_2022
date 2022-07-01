3f058cc79ce0444c1a0e8392a407e497436c8f46
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-01 20:49:45 +0300

    add class Task8

--- src/main/java/students/student_Victor_Goloschapov/lesson_2_homeworks/level_2/Task8.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_2_homeworks/level_2/Task8.java
@@ -1,4 +1,13 @@
 package lesson_2_homeworks.level_2;
+import java.util.Scanner;
 
 public class Task8 {
+    public static  void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please input your name: ");
+        String name = input.nextLine();
+
+        System.out.println("Hello " + name);
+    }
 }
