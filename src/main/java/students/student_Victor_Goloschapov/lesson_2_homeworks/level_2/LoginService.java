c5ff08aad6c006caabb44d116f608154aac89e9d
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-01 20:45:28 +0300

    add modifications comments

--- src/main/java/students/student_Victor_Goloschapov/lesson_2_homeworks/level_2/LoginService.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_2_homeworks/level_2/LoginService.java
@@ -1,4 +1,13 @@
+//Task_7
 package lesson_2_homeworks.level_2;
 
 public class LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
 }
+
+/*Решение
+1.добавил ключевое слово static главному методу
+2.убрал лишние пробелы
+ */
