332bc3c139c428f8197b6e2d6fbafbf5b78138a2
Alisa <sinavskaalisa@gmail.com>
2022-07-02 19:44:04 +0300

    initial commit

--- src/main/java/students/student_Alisa_Sinavska/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package main.java.students.student_Alisa_Sinavska;
-
-public class HelloWorld {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
-    }
-}
