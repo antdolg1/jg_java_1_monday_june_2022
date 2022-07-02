40d2116066f2ab9ee555c508d16dda7164f0f63b
Alisa <sinavskaalisa@gmail.com>
2022-07-02 18:52:44 +0300

    initial commit

---
+++ src/main/java/students/student_alisa_sinavska/_lesson_2/homework/level_2/Task6.java
@@ -0,0 +1,7 @@
+package main.java.students.student_alisa_sinavska._lesson_2.homework.level_2;
+
+class Turtle {
+
+    public static void main(String[] args) {
+}
+}
---
+++ src/main/java/students/student_alisa_sinavska/_lesson_2/homework/level_5/Task18.java
@@ -0,0 +1,11 @@
+package main.java.students.student_alisa_sinavska._lesson_2.homework.level_5;
+
+class SizeMatters {
+
+    public static void main(String[] args) {
+
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
+
