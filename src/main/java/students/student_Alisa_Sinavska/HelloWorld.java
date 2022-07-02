394df129e2b69199c83218bf1fbe60ef91324009
Alisa <sinavskaalisa@gmail.com>
2022-07-02 19:44:31 +0300

    Revert "initial commit"
    This reverts commit 332bc3c139c428f8197b6e2d6fbafbf5b78138a2.

---
+++ src/main/java/students/student_Alisa_Sinavska/HelloWorld.java
@@ -0,0 +1,8 @@
+package main.java.students.student_Alisa_Sinavska;
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
