ec18b4206bf33bee6a383c2a0fce333501b84b61
DenissKaira <dkaira1310@gmail.com>
2022-07-02 17:52:01 +0300

    lesson2_Homework

---
+++ src/main/java/students/student_Denis_Kaira/Lesson_2/Homework/Task8.java
@@ -0,0 +1,12 @@
+package main.java.students.student_Denis_Kaira.Lesson_2.Homework;
+
+import java.util.Scanner;
+public class Task8 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите ваще имя: ");
+        System.out.println("Привет ! " + sc.nextLine());
+        }
+    }
