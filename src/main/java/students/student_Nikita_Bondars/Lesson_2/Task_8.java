d16ee3ab909b956504b237b132a042273ee8a65b
NickBRiga <bondars.nikita@gmail.com>
2022-07-04 00:26:21 +0300

    Add 2nd homework

---
+++ src/main/java/students/student_Nikita_Bondars/Lesson_2/Task_8.java
@@ -0,0 +1,14 @@
+package main.java.students.student_Nikita_Bondars.Lesson_2;
+import java.util.Scanner;
+
+public class Task_8 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+
+        String userName = scanner.next();
+        System.out.println("Hello " + userName);
+
+    }
+}
