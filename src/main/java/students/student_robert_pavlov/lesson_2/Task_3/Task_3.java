5c69b0fec7653341ae065d8d2fb581b315f4aabd
roberts-pavlovs <pavlovsroberts@inbox.lv>
2022-07-03 11:57:59 +0300

    Add my homework Task_3_lesson_2 Скорее всего недопонял задание, получилось вот так.

---
+++ src/main/java/students/student_robert_pavlov/lesson_2/Task_3/Task_3.java
@@ -0,0 +1,17 @@
+package main.java.students.student_robert_pavlov.lesson_2.Task_3;
+
+import java.awt.print.Book;
+
+public class Task_3 {
+    public static void main(String[] args) {
+
+    Book book = new Book();
+
+    System.out.println("New class Book ");
+
+
+
+
+
+    }
+}
