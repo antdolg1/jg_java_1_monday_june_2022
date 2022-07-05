1007dd7e260dfc12993d0f5d82edc6a65aa83c3b
Alisa <sinavskaalisa@gmail.com>
2022-07-05 14:00:14 +0300

    initial commit

---
+++ src/main/java/students/student_Alisa_Sinavska/_lesson_3/level_1/RobotDemo.java
@@ -0,0 +1,16 @@
+package main.java.students.student_alisa_sinavska._lesson_3.level_1;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+
+        Robot robot1 = new Robot(); //Task3
+
+        robot1.sayHello(); // Task4
+        robot1.sayYourName(); //Task5
+
+        Robot robot2 = new Robot(); // Task6
+        robot2.sayHello();
+        robot2.sayYourName();
+
+    }
+}
