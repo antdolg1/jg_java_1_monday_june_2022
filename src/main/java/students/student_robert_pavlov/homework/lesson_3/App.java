25c37a74ad8ea8849646c82519e2a8f2b19fae25
roberts-pavlovs <pavlovsroberts@inbox.lv>
2022-07-06 20:41:04 +0300

    push changes

---
+++ src/main/java/students/student_robert_pavlov/homework/lesson_3/App.java
@@ -0,0 +1,17 @@
+package main.java.students.student_robert_pavlov.homework.lesson_3;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Dog myDog = new Dog();
+
+        int age;
+        String name;
+        int height;
+
+        Dog yourDog = new Dog(age:2, name:"John", height: 30);
+
+    }
+
+}
