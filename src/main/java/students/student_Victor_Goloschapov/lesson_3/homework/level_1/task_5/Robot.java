617072f5542b7b9154526641b2f8bc17a0d5a6d5
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-06 20:26:40 +0300

    add methods

--- src/main/java/students/student_Victor_Goloschapov/lesson_3/homework/level_1/task_5/Robot.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_3/homework/level_1/task_5/Robot.java
@@ -1,10 +1,14 @@
 package lesson_3.homework.level_1.task_5;
 
 class Robot {
-    Robot() {
-
+    String name;
+    Robot(String roboName) {
+        this.name = roboName;
     }
     void sayHello() {
         System.out.println("Hello");
+    }
+    void sayYourName() {
+        System.out.println("My name is: " + name);
     }
 }
--- src/main/java/students/student_Victor_Goloschapov/lesson_3/homework/level_1/task_5/RobotDemo.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_3/homework/level_1/task_5/RobotDemo.java
@@ -2,7 +2,8 @@ package lesson_3.homework.level_1.task_5;
 
 public class RobotDemo {
     public static void main(String[] args) {
-        Robot robot = new Robot();
+        Robot robot = new Robot("ROBO");
         robot.sayHello();
+        robot.sayYourName();
     }
 }
