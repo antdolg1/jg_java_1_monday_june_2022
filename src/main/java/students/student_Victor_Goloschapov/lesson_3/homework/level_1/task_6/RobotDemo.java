cf0402d2d7901fd3f1e7d917ccc11a0ba68a27b5
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-06 20:33:32 +0300

    add Robot objects

--- src/main/java/students/student_Victor_Goloschapov/lesson_3/homework/level_1/task_6/RobotDemo.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_3/homework/level_1/task_6/RobotDemo.java
@@ -2,8 +2,16 @@ package lesson_3.homework.level_1.task_6;
 
 public class RobotDemo {
     public static void main(String[] args) {
-        Robot robot = new Robot("ROBO");
-        robot.sayHello();
-        robot.sayYourName();
+        Robot robot1 = new Robot("ROBO");
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        Robot robot2 = new Robot("ROBO2");
+        robot2.sayHello();
+        robot2.sayYourName();
+
+        Robot robot3 = new Robot("ROBO3");
+        robot3.sayHello();
+        robot3.sayYourName();
     }
 }
