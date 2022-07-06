e71ace42df265a1bd36a951b82857d31ea3a4c25
DenissKaira <dkaira1310@gmail.com>
2022-07-06 17:59:35 +0300

    lesson3_Homework_Task_7-9

---
+++ src/main/java/students/student_Denis_Kaira/Lesson_3/Homework/Task7.java
@@ -0,0 +1,12 @@
+package main.java.students.student_Denis_Kaira.Lesson_3.Homework;
+
+public class Task7 {
+
+     public static void main(String[] args) {
+
+         String myName = "Deniss";
+         System.out.println(myName);
+    }
+
+}
+
---
+++ src/main/java/students/student_Denis_Kaira/Lesson_3/Homework/Task9.java
@@ -0,0 +1,18 @@
+package main.java.students.student_Denis_Kaira.Lesson_3.Homework;
+
+import java.util.Scanner;
+public class Task9 {
+
+
+
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Whats your name: ");
+        String a = in.nextLine();
+        System.out.println("Hello : " + a + "!" );
+
+
+
+    }
+}
