1048b0980497bdd44e60f3ed4f97ba547101910f
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-06 20:24:38 +0300

    initial commit

---
+++ src/main/java/students/student_Victor_Goloschapov/lesson_3/homework/level_1/task_5/RobotDemo.java
@@ -0,0 +1,8 @@
+package lesson_3.homework.level_1.task_5;
+
+public class RobotDemo {
+    public static void main(String[] args) {
+        Robot robot = new Robot();
+        robot.sayHello();
+    }
+}
