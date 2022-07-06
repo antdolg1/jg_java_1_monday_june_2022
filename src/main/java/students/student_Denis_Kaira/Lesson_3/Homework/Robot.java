61a6eedaa44185f09286b14abd96bde7247d2803
DenissKaira <dkaira1310@gmail.com>
2022-07-06 17:23:57 +0300

    lesson3_Homework_Task_1-6

---
+++ src/main/java/students/student_Denis_Kaira/Lesson_3/Homework/Robot.java
@@ -0,0 +1,13 @@
+package main.java.students.student_Denis_Kaira.Lesson_3.Homework;
+
+public class Robot {
+    public static void sayHello(){
+        System.out.println("Hello");
+    }
+    public static void sayNameIsRobo(){
+        System.out.println("My Name Is Robo");
+    }
+
+    public Robot() {
+    }
+}
---
+++ src/main/java/students/student_Denis_Kaira/Lesson_3/Homework/RobotDemo.java
@@ -0,0 +1,18 @@
+package main.java.students.student_Denis_Kaira.Lesson_3.Homework;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+
+
+        Robot myRobot = new Robot();
+        myRobot.sayHello();
+        myRobot.sayNameIsRobo();
+
+        Robot robot2 = new Robot();
+        robot2.sayHello();
+        robot2.sayNameIsRobo();
+
+    }
+}
+
