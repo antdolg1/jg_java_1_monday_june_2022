fc5b15886c4218f6ffb5106e0ca1d5a8a9f42c5e
Nolife777 <edikstrungevich@gmail.com>
2022-07-07 15:36:35 +0300

    Add Task_12

--- src/main/java/students/student_edik_gerasimchuk/Lesson_1/homework/Lesson_1/Task_12.java
+++ src/main/java/students/student_edik_gerasimchuk/Lesson_1/homework/Lesson_1/Task_12.java
@@ -3,11 +3,27 @@ package main.java.students.student_Edik_Gerasimchuk.Lesson_1.homework.Lesson_1;
 public class Task_12 {
 
     public static void main(String[] args) {
-
+// Первый Вариант решения
         int a = 30,
             c = 4,
             d = a * c;
-
+// Второй Вариант решения
         System.out.println("Result is: " + d);
+
+        int b = 30,
+            e = 5;
+
+        System.out.println("Result");
+        System.out.println( b * e );
+// Третий Вариант решения
+        int h = 13;
+
+        int p = 6;
+
+        System.out.println("Result is :");
+        System.out.println( h * p );
+
+
     }
 }
+// Скажите какой из них является правильным (корректным)?
