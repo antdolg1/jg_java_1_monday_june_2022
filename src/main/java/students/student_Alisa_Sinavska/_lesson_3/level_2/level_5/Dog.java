c17ff9b544967f9503a3af4e952b44dac3524016
Alisa <sinavskaalisa@gmail.com>
2022-07-07 22:20:09 +0300

    Lesson_3_level_5

---
+++ src/main/java/students/student_Alisa_Sinavska/_lesson_3/level_2/level_5/Dog.java
@@ -0,0 +1,14 @@
+package main.java.students.student_alisa_sinavska._lesson_3.level_2.level_5;
+
+public class Dog {
+    String name = "Bobik";
+
+    public Dog() {
+
+    }
+
+    void voice() {
+        System.out.println(name + " " + name + " " + name);
+    }
+
+}
