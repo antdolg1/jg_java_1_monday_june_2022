ce3b1beaf49a21d2b8410a3fb4632abda3032744
Nolife777 <edikstrungevich@gmail.com>
2022-07-07 18:04:38 +0300

    Add Task_12

---
+++ src/main/java/students/student_edik_gerasimchuk/Lesson_2/Homework/Lesson_2/LoginService.java
@@ -0,0 +1,9 @@
+package main.java.students.student_Edik_Gerasimchuk.Lesson_2.Homework.Lesson_2;
+
+public class LoginService {
+
+    public static void main(String[] args){
+
+        System.out.println("LoginService");
+    }
+}
