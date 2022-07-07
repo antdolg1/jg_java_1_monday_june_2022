52f2369f6adfb6fed8ea0821af5e86ffe8e87bfa
Nolife777 <edikstrungevich@gmail.com>
2022-07-07 18:19:31 +0300

    Add Task_12

---
+++ src/main/java/students/student_edik_gerasimchuk/Lesson_2/Homework/Lesson_2/Task_8.java
@@ -0,0 +1,16 @@
+package main.java.students.student_Edik_Gerasimchuk.Lesson_2.Homework.Lesson_2;
+import java.util.Scanner;
+
+public class Task_8 {
+
+    public static void main(String[] args){
+
+        Scanner name = new Scanner(System.in);
+        System.out.println("Enter Your Name");
+
+        String userName = name.nextLine();
+        System.out.println("Hello " + userName);
+
+    }
+
+}
