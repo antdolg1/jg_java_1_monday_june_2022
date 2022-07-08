0b062a0a5e3de035bfe1834469542c33f99a1080
roberts-pavlovs <pavlovsroberts@inbox.lv>
2022-07-08 23:06:44 +0300

    Add my homework

---
+++ src/main/java/students/student_robert_pavlov/homework/lesson_3/Task_7/myName.java
@@ -0,0 +1,10 @@
+package main.java.students.student_robert_pavlov.homework.lesson_3.Task_7;
+
+public class myName {
+
+    public static void main(String[] args) {
+
+        String myName = "Robert";
+        System.out.println(myName);
+    }
+}
