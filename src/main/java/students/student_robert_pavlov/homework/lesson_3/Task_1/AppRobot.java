ad5c58a49bd7086ca9cdd7aab4839ac5056fdc7e
roberts-pavlovs <pavlovsroberts@inbox.lv>
2022-07-08 22:59:50 +0300

    push changes

---
+++ src/main/java/students/student_robert_pavlov/homework/lesson_3/Task_1/AppRobot.java
@@ -0,0 +1,18 @@
+package main.java.students.student_robert_pavlov.homework.lesson_3.Task_1;
+
+public class AppRobot {
+
+    public static void main(String[] args) {
+
+        Robot myRobot = new Robot();
+        Robot yourRobot = new Robot();
+
+
+        myRobot.speak();
+        Robot.sayYourName();
+
+        myRobot.speak();
+        Robot.sayYourName2();
+
+        }
+}
---
+++ src/main/java/students/student_robert_pavlov/homework/lesson_3/Task_1/Robot.java
@@ -0,0 +1,23 @@
+package main.java.students.student_robert_pavlov.homework.lesson_3.Task_1;
+
+public class Robot {
+
+    public Robot() {
+
+    }
+
+    public static void speak(){System.out.println("Hello"); }
+    void greetings () {
+        speak();
+    }
+    public static void sayYourName() {System.out.println("My name is Robo");}
+
+    public static void sayYourName2() {System.out.println("My name is Robo2");}
+    // второй робот не получился как было задуманно в задании
+
+
+    }
+
+
+
+
