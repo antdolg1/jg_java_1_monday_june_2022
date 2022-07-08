b5c0c3c12a6ca0bbfb26bfb75738615d1ec7914d
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-08 13:31:25 +0300

    add changes to Task_14

--- src/main/java/students/student_Victor_Goloschapov/lesson_3/homework/Task_14/Book.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_3/homework/Task_14/Book.java
@@ -1,4 +1,27 @@
 package lesson_3.homework.Task_14;
 
-public class Book {
+class Book {
+    String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
 }
+
+class BookDemo {
+
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+    }
+}
+/*Решение
+1. добавил точку с запятой после объявления переменной title, чтобы закрыть инструкцию
+2. переменной title присваиваем значение аргумента функции bookTitle
+3. я так понимаю, что в методе getTitle() можно вернуть значение "return this.title" или "return title"
+ */
