2607d3b4af7397e8b53ad9da8dbb61a1d09f12d8
roberts-pavlovs <pavlovsroberts@inbox.lv>
2022-07-10 09:27:14 +0300

    Add my homework

---
+++ src/main/java/students/student_robert_pavlov/homework/lesson_3/Task_1/Task_2/AppRobot.java
@@ -0,0 +1,15 @@
+package main.java.students.student_robert_pavlov.homework.lesson_3.Task_1.Task_2;
+
+ class AppRobot {
+    public static void main(String[] args) {
+
+        Robot myRobot = new Robot("Rider");
+        Robot robot1 = new Robot("John");
+
+        myRobot.sayHello();
+        myRobot.sayYourName();
+
+        robot1.sayHello();
+        robot1.sayYourName();
+    }
+}
---
+++ src/main/java/students/student_robert_pavlov/homework/lesson_3/Task_1/Task_2/Robot.java
@@ -0,0 +1,17 @@
+package main.java.students.student_robert_pavlov.homework.lesson_3.Task_1.Task_2;
+
+ class Robot {
+
+     String robotName;
+     public Robot (String robotName) {this.robotName = robotName;}
+     void sayYourName (String robotName) {
+         System.out.println(" My name is " + robotName);
+         this.robotName = robotName;
+         }
+         void speak () {System.out.println(" Hello "); }
+    void sayHello () {
+    }
+
+      void sayYourName() {
+     }
+ }
