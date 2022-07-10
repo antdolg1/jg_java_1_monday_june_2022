75645e78fce7080cf625c03e6f0bded3fefe3db6
roberts-pavlovs <pavlovsroberts@inbox.lv>
2022-07-10 09:34:44 +0300

    Add my homework

---
+++ src/main/java/students/student_robert_pavlov/homework/lesson_3/Lesson_3Task_9.java
@@ -0,0 +1,14 @@
+package main.java.students.student_robert_pavlov.homework.lesson_3;
+
+import java.util.Scanner;
+
+public class Lesson_3Task_9 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Hello user: ");
+        int num = in.nextInt();
+
+
+    }
+}
