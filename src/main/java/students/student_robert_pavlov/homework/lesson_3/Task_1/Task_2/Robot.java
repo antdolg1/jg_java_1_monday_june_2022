d8277af4f6fbe3c1676830d7286ab9450387ba0f
roberts-pavlovs <pavlovsroberts@inbox.lv>
2022-07-10 11:01:17 +0300

    change and add my homework

--- src/main/java/students/student_robert_pavlov/homework/lesson_3/Task_1/Task_2/Robot.java
+++ src/main/java/students/student_robert_pavlov/homework/lesson_3/Task_1/Task_2/Robot.java
@@ -3,15 +3,12 @@ package main.java.students.student_robert_pavlov.homework.lesson_3.Task_1.Task_2
  class Robot {
 
      String robotName;
-     public Robot (String robotName) {this.robotName = robotName;}
-     void sayYourName (String robotName) {
-         System.out.println(" My name is " + robotName);
-         this.robotName = robotName;
-         }
-         void speak () {System.out.println(" Hello "); }
-    void sayHello () {
-    }
+      Robot (String robotName) {this.robotName = robotName;}
 
-      void sayYourName() {
+     void sayYourName() {
+         System.out.println(" My name is " + this.robotName);
      }
+
+         void sayHello () {System.out.println(" Hello "); }
+
  }
