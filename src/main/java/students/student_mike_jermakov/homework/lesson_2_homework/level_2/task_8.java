132ec0299042e24ee9cbd5a2a74f6002f9e996c7
m1keus <mike@jermakov.com>
2022-07-11 18:52:12 +0300

    lesson_2_homework.level_1

---
+++ src/main/java/students/student_mike_jermakov/homework/lesson_2_homework/level_2/task_8.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Task8 {
+
+    public static void main(String[]args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write user name");
+        String userName = scanner.next();
+
+        System.out.println("Hello " + userName);
+    }
+}
