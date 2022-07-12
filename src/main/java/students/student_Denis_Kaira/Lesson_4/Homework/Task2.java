411b0ed968ec0abd3c79f1f95de27e9384b07e78
DenissKaira <dkaira1310@gmail.com>
2022-07-12 21:57:41 +0300

    Homework_task_1-7

---
+++ src/main/java/students/student_Denis_Kaira/Lesson_4/Homework/Task2.java
@@ -0,0 +1,22 @@
+package main.java.students.student_Denis_Kaira.Lesson_4.Homework;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (number == 0) {
+            System.out.println("Number is equal to zero");
+        } else if (number > 0) {
+            System.out.println("Number is positive");
+        } else if (number < 0) {
+            System.out.println("Number is negative");
+        }
+
+    }
+}
---
+++ src/main/java/students/student_Denis_Kaira/Lesson_4/Homework/Task5.java
@@ -0,0 +1,21 @@
+package main.java.students.student_Denis_Kaira.Lesson_4.Homework;
+
+import java.util.Scanner;
+
+public class Task5 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number 1 : ");
+        int number = scanner.nextInt();
+        System.out.println("Enter another number 2 : ");
+        int number2 = scanner.nextInt();
+
+        if (number > number2) {
+            System.out.print( number );
+        } else {
+            System.out.print( number2 );
+        }
+    }
+}
