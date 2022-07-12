98339c49d25bf5651867dc7ec46607da9d60db4f
leonards205 <leonadrds.petkevics@gmail.com>
2022-07-12 15:55:02 +0300

    Lesson 4

---
+++ src/main/java/students/student_leonards_petkevics/lesson_4/IfElseIf.java
@@ -0,0 +1,31 @@
+package main.java.students.student_leonards_petkevics.lesson_4;
+
+class IfElseIf {
+    public static void main(String[] args) {
+
+//        String catName = "bob";
+//
+//        if (catName.equals("Lui")) {
+//            System.out.println("Your cat name is Lui!");
+//        }
+//        else if (catName.equals("John")) {
+//            System.out.println("Your cat name is John");
+//        }
+//        else if (catName.equals("Dora")) {
+//            System.out.println("Your cat name is Dora!");
+//        }
+//        else {
+//            System.out.println("You don't have a cat!");
+//        }
+//        int x = 10;
+//        if (!(x > 2)){
+//            System.out.println("x > 2");
+//        }
+//        else {
+//            System.out.println("x < 2");
+//        }
+//        String creditCard = "VISA";
+//        if (creditCard.equals("MASTERCARD") ) ||
+
+     }
+    }
