d8da807343f17a54068699c6ac4f7f3de80e420a
Nolife777 <edikstrungevich@gmail.com>
2022-07-13 14:18:35 +0300

    Add Task

---
+++ src/main/java/students/student_edik_gerasimchuk/Lesson_4/Level_1/Task_1.java
@@ -0,0 +1,23 @@
+package main.java.students.student_Edik_Gerasimchuk.Lesson_4.Level_1;
+
+import java.util.Scanner;
+
+public class Number {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello");
+
+        System.out.println("Write an integer :");
+        int firstNumber = scanner.nextInt();
+
+        if (firstNumber >= 0) {
+            System.out.println("Your number is POSITIVE");
+        } else {
+            System.out.println("Your number is NEGATIVE");
+        }
+    }
+}
+//Task 1
