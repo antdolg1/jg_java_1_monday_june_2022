1804975e51d994d21982cf6cb33f639a50c807d6
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-14 13:49:44 +0300

    add color test logic

--- src/main/java/students/student_Victor_Goloschapov/lesson_4/homework/level_5/Task16/LightColorDetectorTest.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_4/homework/level_5/Task16/LightColorDetectorTest.java
@@ -4,25 +4,115 @@ import java.util.Scanner;
 
 public class LightColorDetectorTest {
     public static void main(String[] args) {
-        //app logic
+        Scanner input = new Scanner(System.in);
+        int lightWaveLength;
+
+        System.out.print("Please enter light wave length: ");
+
+        lightWaveLength = input.nextInt();
+
+        LightColorDetectorTest colorTest = new LightColorDetectorTest();
+        colorTest.getVioletColor(lightWaveLength);
+        colorTest.getBlueColor(lightWaveLength);
+        colorTest.getGreenColor(lightWaveLength);
+        colorTest.getYellowColor(lightWaveLength);
+        colorTest.getOrangeColor(lightWaveLength);
+        colorTest.getRedColor(lightWaveLength);
     }
-    //test methods
-    void getVioletColor() {
-        //method code
+
+    void getVioletColor(int waveLength) {
+        LightColorDetector color = new LightColorDetector();
+        String expectedResult = "Violet";
+        String actualResult = color.detect(waveLength);
+
+        if (expectedResult.equals(actualResult)) {
+            System.out.println("***Test passed. Expected color: " + expectedResult);
+            System.out.println("======================================");
+        } else {
+            System.out.println("Test failed");
+            System.out.println("Actual result: " + actualResult);
+            System.out.println("Expected result: " + expectedResult);
+            System.out.println("======================================");
+        }
     }
-    void getBlueColor() {
-        //method code
+
+    void getBlueColor(int waveLength) {
+        LightColorDetector color = new LightColorDetector();
+        String expectedResult = "Blue";
+        String actualResult = color.detect(waveLength);
+
+        if (expectedResult.equals(actualResult)) {
+            System.out.println("***Test passed. Expected color: " + expectedResult);
+            System.out.println("======================================");
+        } else {
+            System.out.println("Test failed");
+            System.out.println("Actual result: " + actualResult);
+            System.out.println("Expected result: " + expectedResult);
+            System.out.println("======================================");
+        }
     }
-    void getGreenColor() {
-        //method code
+
+    void getGreenColor(int waveLength) {
+        LightColorDetector color = new LightColorDetector();
+        String expectedResult = "Green";
+        String actualResult = color.detect(waveLength);
+
+        if (expectedResult.equals(actualResult)) {
+            System.out.println("***Test passed. Expected color: " + expectedResult);
+            System.out.println("======================================");
+        } else {
+            System.out.println("Test failed");
+            System.out.println("Actual result: " + actualResult);
+            System.out.println("Expected result: " + expectedResult);
+            System.out.println("======================================");
+        }
     }
-    void getYellowColor() {
-        //method code
+
+    void getYellowColor(int waveLength) {
+        LightColorDetector color = new LightColorDetector();
+        String expectedResult = "Yellow";
+        String actualResult = color.detect(waveLength);
+
+        if (expectedResult.equals(actualResult)) {
+            System.out.println("***Test passed. Expected color: " + expectedResult);
+            System.out.println("======================================");
+        } else {
+            System.out.println("Test failed");
+            System.out.println("Actual result: " + actualResult);
+            System.out.println("Expected result: " + expectedResult);
+            System.out.println("======================================");
+        }
     }
-    void getOrangeColor() {
-        //method code
+
+    void getOrangeColor(int waveLength) {
+        LightColorDetector color = new LightColorDetector();
+        String expectedResult = "Orange";
+        String actualResult = color.detect(waveLength);
+
+        if (expectedResult.equals(actualResult)) {
+            System.out.println("***Test passed. Expected color: " + expectedResult);
+            System.out.println("======================================");
+        } else {
+            System.out.println("Test failed");
+            System.out.println("Actual result: " + actualResult);
+            System.out.println("Expected result: " + expectedResult);
+            System.out.println("======================================");
+        }
     }
-    void getRedColor() {
-        //method code
+
+    void getRedColor(int waveLength) {
+        LightColorDetector color = new LightColorDetector();
+        String expectedResult = "Red";
+        String actualResult = color.detect(waveLength);
+
+        if (expectedResult.equals(actualResult)) {
+            System.out.println("***Test passed. Expected color: " + expectedResult);
+            System.out.println("======================================");
+        } else {
+            System.out.println("Test failed");
+            System.out.println("Actual result: " + actualResult);
+            System.out.println("Expected result: " + expectedResult);
+            System.out.println("======================================");
+        }
     }
 }
