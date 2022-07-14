deed4d2bb831ed705d505131fd0d4be06e1a9092
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-14 12:33:47 +0300

    add app - show smalest digit

---
+++ src/main/java/students/student_Victor_Goloschapov/lesson_4/homework/level_2/Task_6.java
@@ -0,0 +1,23 @@
+package lesson_4.homework.level_2;
+
+import java.util.Scanner;
+
+public class Task_6 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number1;
+        int number2;
+
+        System.out.print("Введите целое число 1: ");
+        number1 = input.nextInt();
+
+        System.out.print("Введите целое число 2: ");
+        number2 = input.nextInt();
+
+        if (number1 < number2) {
+            System.out.print("Ответ: " + number1);
+        } else if (number2 < number1) {
+            System.out.print("Ответ: " + number2);
+        }
+    }
+}
