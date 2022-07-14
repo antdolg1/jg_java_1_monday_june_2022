869104f1461f9e005d15ab8dfff62c78e95304ed
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-14 12:27:31 +0300

    add odd numbers check

--- src/main/java/students/student_Victor_Goloschapov/lesson_4/homework/level_1/Task_4.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_4/homework/level_1/Task_4.java
@@ -1,4 +1,19 @@
 package lesson_4.homework.level_1;
 
+import java.util.Scanner;
+
 public class Task_4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+
+        System.out.print("Введите целое число: ");
+        number = input.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("Число четное");
+        } else {
+            System.out.println("Число нечетное");
+        }
+    }
 }
