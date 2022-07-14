7b15ac5ef464535b7e9870fc33d3767bd873cb26
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-14 13:03:38 +0300

    add check which digit is bigger

--- src/main/java/students/student_Victor_Goloschapov/lesson_4/homework/level_3/Task10.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_4/homework/level_3/Task10.java
@@ -1,4 +1,29 @@
 package lesson_4.homework.level_3;
 
+import java.util.Scanner;
+
 public class Task10 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number1;
+        int number2;
+        int number3;
+
+        System.out.print("Введите целое число 1: ");
+        number1 = input.nextInt();
+
+        System.out.print("Введите целое число 2: ");
+        number2 = input.nextInt();
+
+        System.out.print("Введите целое число 3: ");
+        number3 = input.nextInt();
+
+        if (number1 > number2 && number1 > number3) {
+            System.out.print("Ответ число 1: " + number1);
+        } else if (number2 > number1 && number2 > number3) {
+            System.out.print("Ответ число 2: " + number2);
+        } else if (number3 > number2 && number3 > number1) {
+            System.out.print("Ответ число 3: " + number3);
+        }
+    }
 }
