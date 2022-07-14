8aabb5c20ec9fb6e53e97cbb051e76b05746c3e1
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-14 12:16:21 +0300

    add switch case to show weekday ID

---
+++ src/main/java/students/student_Victor_Goloschapov/lesson_4/homework/level_1/Task_3.java
@@ -0,0 +1,39 @@
+package lesson_4.homework.level_1;
+
+import java.util.Scanner;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+
+        System.out.print("Please enter number from 1 to 7 which is equals a week day: ");
+        number = input.nextInt();
+
+        switch (number) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("There are only 7 days in a week. Please enter correct number");
+        }
+    }
+}
