8ee3310329d9c19bbd477410155e17e9f83452b2
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-14 13:11:26 +0300

    initial commit

---
+++ src/main/java/students/student_Victor_Goloschapov/lesson_4/homework/level_5/Task16/LightColorDetectorTest.java
@@ -0,0 +1,10 @@
+package lesson_4.homework.level_5.Task16;
+
+import java.util.Scanner;
+
+public class LightColorDetectorTest {
+    public static void main(String[] args) {
+        //app logic
+    }
+    //test methods
+}
