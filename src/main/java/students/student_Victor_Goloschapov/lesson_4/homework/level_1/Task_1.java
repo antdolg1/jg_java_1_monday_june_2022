152e14629892acb7ae45d0c97bc357e871b5254a
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-14 12:10:10 +0300

    add if-else for digit check

--- src/main/java/students/student_Victor_Goloschapov/lesson_4/homework/level_1/Task_1.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_4/homework/level_1/Task_1.java
@@ -1,4 +1,24 @@
 package lesson_4.homework.level_1;
 
+import java.util.Scanner;
+
 public class Task_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+
+        System.out.print("Введите число ");
+
+        if (input.hasNextInt()) {
+            number = input.nextInt();
+            if (number < 0) {
+                System.out.println("Число " + number + " отрицательное");
+            } else {
+                System.out.println("Число " + number + " положительное");
+            }
+        } else {
+            System.out.println("Извините, но это явно не число. Введите число");
+        }
+    }
 }
+//Решил добавить проверку на правильность ввода данных
