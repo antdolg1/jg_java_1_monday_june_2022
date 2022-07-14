b764e28b14609a0e64e18757eb409297106173bc
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-14 12:59:44 +0300

    add increasing/decreasing check

--- src/main/java/students/student_Victor_Goloschapov/lesson_4/homework/level_3/Task9.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_4/homework/level_3/Task9.java
@@ -1,4 +1,29 @@
 package lesson_4.homework.level_3;
 
+import java.util.Scanner;
+
 public class Task9 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number1;
+        int number2;
+        int number3;
+
+        System.out.print("Введите целое число 1: ");
+        number1 = input.nextInt();
+
+        System.out.print("Введите целое число 2: ");
+        number2 = input.nextInt();
+
+        System.out.print("Введите целое число 3: ");
+        number3 = input.nextInt();
+
+        if (number1 < number2 && number2 < number3) {
+            System.out.println("Increasing order");
+        } else if (number1 > number2 && number2 > number3) {
+            System.out.println("Decreasing order");
+        } else {
+            System.out.println("Neither increasing or decreasing order");
+        }
+    }
 }
