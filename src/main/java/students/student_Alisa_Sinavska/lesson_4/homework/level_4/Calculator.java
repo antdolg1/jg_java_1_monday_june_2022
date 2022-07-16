d9a3e8f83e7801d73a79b4e3f086dc3e1c14759c
Alisa <sinavskaalisa@gmail.com>
2022-07-16 23:01:44 +0300

    Lesson_4_level_5

--- src/main/java/students/student_Alisa_Sinavska/lesson_4/homework/level_4/Calculator.java
+++ src/main/java/students/student_Alisa_Sinavska/lesson_4/homework/level_4/Calculator.java
@@ -4,32 +4,38 @@ public class Calculator {
     public int sum(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
     }
+
     public int dif(int firstNumber, int secondNumber) {
         return firstNumber - secondNumber;
     }
+
     public int mult(int firstNumber, int secondNumber) {
         return firstNumber * secondNumber;
     }
+
     public int div(int firstNumber, int secondNumber) {
         return firstNumber / secondNumber;
     }
+
     public boolean isEven(int number) {
         if (number % 2 == 0) {
             return true;
         } else {
             return false;
         }
-        }
-        public String maxOfTwoNumbers(int firstNumber, int secondNumber){
+    }
+
+    public String maxOfTwoNumbers(int firstNumber, int secondNumber) {
         if (firstNumber > secondNumber) {
-        return "Max of two numbers is " + firstNumber;
+            return "Max of two numbers is " + firstNumber;
         } else if (secondNumber > firstNumber) {
-        return  "Max of two numbers is " + secondNumber;
+            return "Max of two numbers is " + secondNumber;
         } else {
-            return "numbers are equals";
+            return "numbers are equal";
         }
-        }
-    public int maxOfThreeNumbers(int firstNumber, int secondNumber, int thirdNumber){
+    }
+
+    public int maxOfThreeNumbers(int firstNumber, int secondNumber, int thirdNumber) {
         if (firstNumber > secondNumber && firstNumber > thirdNumber) {
             return firstNumber;
         } else if (secondNumber > firstNumber && secondNumber > thirdNumber) {
@@ -47,11 +53,12 @@ public class Calculator {
         } else if (secondNumber > thirdNumber && thirdNumber == firstNumber) {
             return secondNumber;
         } else if (thirdNumber > secondNumber && secondNumber == firstNumber) {
-            return secondNumber;
-        } else  {
-                return secondNumber;
+            return thirdNumber;
+        } else if (secondNumber == firstNumber && firstNumber == thirdNumber) {
         }
+        return firstNumber;
     }
+}
 
-    }
+
 
---
+++ src/main/java/students/student_Alisa_Sinavska/lesson_4/homework/level_5/LightColorDetector.java
@@ -0,0 +1,23 @@
+package main.java.students.student_alisa_sinavska.lesson_4.homework.level_5;
+
+public class LightColorDetector {
+    public String detect(int wavelength) {
+        if (wavelength >= 380 && wavelength < 450) {
+            return "Violet";
+        } else if (wavelength >= 450 && wavelength < 495) {
+            return "Blue";
+        } else if (wavelength >= 495 && wavelength < 570){
+            return "Green";
+        } else if (wavelength >= 570 && wavelength < 590){
+            return "Yellow";
+        } else if (wavelength >= 590 && wavelength < 620){
+            return "Orange";
+        } else if (wavelength >= 620 && wavelength <= 750){
+            return "Red";
+        } else {
+            return "Invisible Light";
+        }
+        }
+
+    }
+
