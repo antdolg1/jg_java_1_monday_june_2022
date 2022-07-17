7174fa09b98ebff9d498e49e375806f6899888e4
Alisa <sinavskaalisa@gmail.com>
2022-07-17 15:37:06 +0300

    Lesson_4_changes

--- src/main/java/students/student_Alisa_Sinavska/_lesson_3/level_2/level_7/BottleOfWine.java
+++ src/main/java/students/student_Alisa_Sinavska/_lesson_3/level_2/level_7/BottleOfWine.java
@@ -2,11 +2,11 @@ package main.java.students.student_alisa_sinavska._lesson_3.level_2.level_7;
 
 class BottleOfWine {
 
-    String collor;
+    String color;
     double volume;
     int quantity;
 
-    public BottleOfWine(String collor) {
+    public BottleOfWine(String color) {
     this.volume = 750.0;
     this.quantity = 2;
 }
