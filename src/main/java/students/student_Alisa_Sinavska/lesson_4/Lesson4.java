a7392f6f29dfbd87d513501f4c878ab65a066694
Alisa <sinavskaalisa@gmail.com>
2022-07-17 15:23:55 +0300

    Lesson_4_changes

--- src/main/java/students/student_Alisa_Sinavska/lesson_4/Lesson4.java
+++ src/main/java/students/student_Alisa_Sinavska/lesson_4/Lesson4.java
@@ -4,7 +4,7 @@ public class Lesson4 {
     public static void main(String[] args) {
 
     int number = 18;
-    if (number >= 20) {
+    if (number < 20) {
         System.out.println("number is less than 20");
     }
 }
