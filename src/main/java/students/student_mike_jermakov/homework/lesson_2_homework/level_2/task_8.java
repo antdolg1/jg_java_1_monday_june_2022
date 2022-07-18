143cd5febcee12064ee5546a50e60ba634a62b43
Antons Dolgopolovs <antdolg@gmail.com>
2022-07-18 12:34:37 +0300

    Small fixes

--- src/main/java/students/student_mike_jermakov/homework/lesson_2_homework/level_2/task_8.java
+++ src/main/java/students/student_mike_jermakov/homework/lesson_2_homework/level_2/task_8.java
@@ -1,14 +1,14 @@
 
 import java.util.Scanner;
 
-public class Task8 {
+class Task8 {
 
-    public static void main(String[]args) {
+    public static void main(final String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        final Scanner scanner = new Scanner(System.in);
 
         System.out.println("Write user name");
-        String userName = scanner.next();
+        final String userName = scanner.next();
 
         System.out.println("Hello " + userName);
     }
