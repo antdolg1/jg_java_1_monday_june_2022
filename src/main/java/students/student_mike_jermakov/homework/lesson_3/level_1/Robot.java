5a305691f88cd2c2acd7029a61b775b041109e03
m1keus <mike@jermakov.con>
2022-07-19 21:38:33 +0300

    lesson 3 level 1 homework

---
+++ src/main/java/students/student_mike_jermakov/homework/lesson_3/level_1/Robot.java
@@ -0,0 +1,17 @@
+package main.java.students.student_mike_jermakov.homework.lesson_3.level_1;
+
+class Robot {
+
+    public static void sayHello() {
+        System.out.println("Hello!");
+    }
+
+    public static void sayYourName() {
+        System.out.println("My name is Robo!");
+    }
+
+    public Robot() {
+
+    }
+
+}
