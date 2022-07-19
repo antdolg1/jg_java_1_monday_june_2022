bd13934c82a4bef1b344d541d3f06d86fd08ba8c
m1keus <mike@jermakov.con>
2022-07-19 21:43:33 +0300

    lesson 3 level 2 homework

---
+++ src/main/java/students/student_mike_jermakov/homework/lesson_3/level_2/Task_8.java
@@ -0,0 +1,12 @@
+package main.java.students.student_mike_jermakov.homework.lesson_3.level_2;
+
+public class Task_8 {
+    public static void main(String[] args) {
+
+        String userName = "Mike";
+        String greeting = "Hi " + userName + "!";
+        System.out.println(greeting);
+
+    }
+
+}
