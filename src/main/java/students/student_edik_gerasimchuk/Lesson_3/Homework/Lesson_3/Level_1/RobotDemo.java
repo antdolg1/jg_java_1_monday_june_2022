748d3b0b93b6368fe016fd83b52055d13486be1a
Nolife777 <edikstrungevich@gmail.com>
2022-07-20 17:43:58 +0300

    Add Task

---
+++ src/main/java/students/student_edik_gerasimchuk/Lesson_3/Homework/Lesson_3/Level_1/RobotDemo.java
@@ -0,0 +1,18 @@
+package main.java.students.student_Edik_Gerasimchuk.Lesson_3.Homework.Lesson_3;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+
+       Robot myRobot = new Robot();
+
+
+       myRobot.sayHello();
+       myRobot.sayYourName();
+
+       Robot myRobot2 = new Robot();
+
+       myRobot2.sayHello();
+       myRobot2.sayYourName();
+    }
+}
---
+++ src/main/java/students/student_edik_gerasimchuk/Lesson_3/Homework/Lesson_3/Level_2/Task_7.java
@@ -0,0 +1,10 @@
+package main.java.students.student_Edik_Gerasimchuk.Lesson_3.Level_2;
+
+public class Task_7 {
+
+    public static void main(String[] args) {
+
+        String myName = "Eddy";
+        System.out.println(myName);
+    }
+}
