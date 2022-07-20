8e24eb32627e4bb470107d63680ea7b1da9e9ac6
Nolife777 <edikstrungevich@gmail.com>
2022-07-20 17:23:02 +0300

    Add Task

---
+++ src/main/java/students/student_edik_gerasimchuk/Lesson_3/Homework/Lesson_3/Level_1/Robot.java
@@ -0,0 +1,18 @@
+package main.java.students.student_Edik_Gerasimchuk.Lesson_3.Homework.Lesson_3;
+
+class Robot {
+
+    public Robot() {
+
+    }
+
+    public void sayHello() {
+
+        System.out.println("Hello");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO!");
+
+    }
+}
