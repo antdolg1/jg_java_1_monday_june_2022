9177be41b4868439fbaebeca0607ac05bdae76ba
Nolife777 <edikstrungevich@gmail.com>
2022-07-20 17:20:14 +0300

    Add Task

---
+++ src/main/java/students/student_edik_gerasimchuk/Coding.java
@@ -0,0 +1,19 @@
+package main.java.students.student_Edik_Gerasimchuk;
+
+import java.util.Scanner;
+
+public class Coding {
+
+    public static void main(String[] args) {
+
+
+    }
+}
+
+
+
+
+
+
+
+
