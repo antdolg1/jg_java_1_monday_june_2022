6e32247b95e40b1fdfa47183d66330e7347e1108
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-22 22:19:07 +0300

    add Task9 solution

---
+++ src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_1/Task_9/ArrayLength3.java
@@ -0,0 +1,12 @@
+package lesson_5.homework.level_1.Task_9;
+
+public class ArrayLength3 {
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers.length);
+    }
+}
+/*Решение
+1. Добавил [] после int
+2. У свойства length убрал скобки.
+*/
