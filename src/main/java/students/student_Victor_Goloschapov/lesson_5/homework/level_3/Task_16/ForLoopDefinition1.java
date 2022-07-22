26467d0cf704eba68892bf4a6247554aded4640d
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-22 22:57:13 +0300

    add task16 solution

--- src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_3/Task_16/ForLoopDefinition1.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_3/Task_16/ForLoopDefinition1.java
@@ -1,4 +1,12 @@
 package lesson_5.homework.level_3.Task_16;
 
 public class ForLoopDefinition1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
 }
+/*Решение
+1. переменной i присвоил значение 0
+*/
