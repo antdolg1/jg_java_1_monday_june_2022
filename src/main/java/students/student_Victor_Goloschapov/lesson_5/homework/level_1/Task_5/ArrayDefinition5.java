1f414ed1fda43405d29c92570ee04df18bff48b0
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-22 22:08:34 +0300

    add Task5 solution

--- src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_1/Task_5/ArrayDefinition5.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_1/Task_5/ArrayDefinition5.java
@@ -1,4 +1,12 @@
 package lesson_5.homework.level_1.Task_5;
 
 public class ArrayDefinition5 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4};
+    }
 }
+
+/* Решение
+1. [] поставил после int
+2/ добавил четвертый элемент в массив и убрал лишнюю запятую
+*/
