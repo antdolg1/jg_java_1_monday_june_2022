b2635982dde080f9993e061e9576ea1a64e4aad6
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-22 22:33:15 +0300

    correction data type for array

--- src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_2/Task12.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_2/Task12.java
@@ -2,10 +2,10 @@ package lesson_5.homework.level_2;
 
 public class Task12 {
     public static void main(String[] args) {
-        double[] randomNumberArray = new double[3];
+        int[] randomNumberArray = new int[3];
 
         for (int i = 0; i < randomNumberArray.length; i++) {
-            randomNumberArray[i] = Math.rint(Math.random() * 10);
+            randomNumberArray[i] = (int) Math.rint(Math.random() * 100);
             System.out.println("Результат вывода на консоль: " + randomNumberArray[i]);
         }
     }
