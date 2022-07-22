361b288c8822733cdfa4f43de32c2f8df15d6009
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-22 23:04:55 +0300

    add task22 solution

---
+++ src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_3/Task_22/ForLoopDefinition6.java
@@ -0,0 +1,12 @@
+package lesson_5.homework.level_3.Task_22;
+
+public class ForLoopDefinition6 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+/*Решение
+1. добавил скобку перед блоком с условием
+*/
