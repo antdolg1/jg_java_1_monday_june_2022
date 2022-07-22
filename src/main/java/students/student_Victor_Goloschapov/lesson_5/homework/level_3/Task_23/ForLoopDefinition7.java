f53d48eb0014651da464e7c0a34cc6c520f317c2
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-22 23:06:17 +0300

    add task23 solution

---
+++ src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_3/Task_23/ForLoopDefinition7.java
@@ -0,0 +1,12 @@
+package lesson_5.homework.level_3.Task_23;
+
+public class ForLoopDefinition7 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+/*Решение
+1. добавил закрывающую } после тела цикла
+*/
