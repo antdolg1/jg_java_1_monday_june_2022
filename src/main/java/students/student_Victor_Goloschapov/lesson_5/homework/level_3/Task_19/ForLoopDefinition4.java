0de256465c3d3706b47e576db0111580df5821e2
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-22 23:01:24 +0300

    add task19 solution

---
+++ src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_3/Task_19/ForLoopDefinition4.java
@@ -0,0 +1,12 @@
+package lesson_5.homework.level_3.Task_19;
+
+public class ForLoopDefinition4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+/*Решение
+1. поменял инкремент на i++
+*/
