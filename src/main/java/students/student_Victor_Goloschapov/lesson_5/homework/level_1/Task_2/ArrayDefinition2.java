09aa815d678a1e7b11ad7475c9fe50ad07c818c3
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-22 22:01:00 +0300

    add task2 solution

--- src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_1/Task_2/ArrayDefinition2.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_1/Task_2/ArrayDefinition2.java
@@ -1,4 +1,11 @@
 package lesson_5.homework.level_1.Task_2;
 
 public class ArrayDefinition2 {
+    public static void main(String[] args) {
+        byte[] numbers = new byte[10];
+    }
 }
+
+/*Решение
+1. поменял тип данных при объявлении переменной на byte и добавил [] после типа данных
+ */
