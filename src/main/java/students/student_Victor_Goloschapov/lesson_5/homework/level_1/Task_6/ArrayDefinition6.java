132fea3870c3c66750bc1cf10642842b8d8c2241
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-22 22:10:31 +0300

    add Task6 solution

--- src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_1/Task_6/ArrayDefinition6.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_1/Task_6/ArrayDefinition6.java
@@ -1,4 +1,11 @@
 package lesson_5.homework.level_1.Task_6;
 
 public class ArrayDefinition6 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+    }
 }
+/*Решение
+1. Добавил [] после int
+2. в массиве убрал запятую и добавил закрывающую скобку }
+*/
