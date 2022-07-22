b5d5d2a4ec752ee9a9b5f1c3acdd273c15489ea2
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-22 22:26:06 +0300

    add Task12 solution

---
+++ src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_2/Task12.java
@@ -0,0 +1,12 @@
+package lesson_5.homework.level_2;
+
+public class Task12 {
+    public static void main(String[] args) {
+        double[] randomNumberArray = new double[3];
+
+        for (int i = 0; i < randomNumberArray.length; i++) {
+            randomNumberArray[i] = Math.random();
+            System.out.println("Результат вывода на консоль: " + randomNumberArray[i]);
+        }
+    }
+}
