a89030f4da38742acde22908d10dde9e59ba3257
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-22 22:04:29 +0300

    add Task3 solution

--- src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_1/Task_3/ArrayDefinition3.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_1/Task_3/ArrayDefinition3.java
@@ -1,4 +1,11 @@
 package lesson_5.homework.level_1.Task_3;
 
 public class ArrayDefinition3 {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+    }
 }
+/*Решение
+1. поставил тип данных int вместо boolean
+2. добавил [] после int
+*/
