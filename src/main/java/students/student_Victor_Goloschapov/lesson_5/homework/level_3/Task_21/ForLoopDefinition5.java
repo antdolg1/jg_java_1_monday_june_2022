ec2df978e7d9cf5cde8f919fc133dce9bd1e63de
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-22 23:03:32 +0300

    add task21 solution

---
+++ src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_3/Task_21/ForLoopDefinition5.java
@@ -0,0 +1,12 @@
+package lesson_5.homework.level_3.Task_21;
+
+public class ForLoopDefinition5 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+/*Решение
+1. логическую ошибку не нашел, условие работает, но добавил { после условия
+*/
