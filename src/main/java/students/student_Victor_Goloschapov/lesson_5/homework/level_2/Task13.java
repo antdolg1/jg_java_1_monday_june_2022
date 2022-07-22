7148da59a8e661d24d41fe213795258221e77dbf
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-22 22:37:40 +0300

    task13 final solution

--- src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_2/Task13.java
+++ src/main/java/students/student_Victor_Goloschapov/lesson_5/homework/level_2/Task13.java
@@ -1,4 +1,15 @@
 package lesson_5.homework.level_2;
 
 public class Task13 {
+    public static void main(String[] args) {
+        int[] randomNumberArray = new int[3];
+        int rangeTill100 = 100;
+        int sumResult = 0;
+
+        for (int i = 0; i < randomNumberArray.length; i++) {
+            randomNumberArray[i] = (int) (Math.random() * rangeTill100);
+            sumResult += randomNumberArray[i];
+        }
+        System.out.println("Результат сложения чисел в массиве: " + sumResult);
+    }
 }
