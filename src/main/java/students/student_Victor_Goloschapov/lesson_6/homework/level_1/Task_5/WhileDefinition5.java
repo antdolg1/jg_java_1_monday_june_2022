db6d42a7a57cdb9c51b250a17bfd173f8b6c069e
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-26 20:48:36 +0300

    add task_5 solution

---
+++ src/main/java/students/student_Victor_Goloschapov/lesson_6/homework/level_1/Task_5/WhileDefinition5.java
@@ -0,0 +1,15 @@
+package lesson_6.homework.level_1.Task_5;
+
+public class WhileDefinition5 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+/*Решение
+1. Условие i <= 100
+2. Инкремент увеличивает на 1 единицу после каждой итерации
+*/
