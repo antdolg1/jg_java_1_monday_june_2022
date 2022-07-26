b00b16b247c36e50b815312a31cc38fbb90e634b
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-26 20:38:55 +0300

    add task_2 solution

---
+++ src/main/java/students/student_Victor_Goloschapov/lesson_6/homework/level_1/Task_2/WhileDefinition2.java
@@ -0,0 +1,14 @@
+package lesson_6.homework.level_1.Task_2;
+
+public class WhileDefinition2 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+/*Решение
+1. Добавил { после условия
+ */
