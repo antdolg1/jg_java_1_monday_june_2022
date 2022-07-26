9834ac6c42dc6195142a847093fa589292bf03ef
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-26 20:36:35 +0300

    add task_1 solution

---
+++ src/main/java/students/student_Victor_Goloschapov/lesson_6/homework/level_1/Task_1/WhileDefinition1.java
@@ -0,0 +1,14 @@
+package lesson_6.homework.level_1.Task_1;
+
+public class WhileDefinition1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+/*Решение
+1. Добавил скобку перед условием
+ */
