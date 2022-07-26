21a5322d505f54cbe089058e4811c48e4844480c
Victor_Goloschapov <victor.goloschapov@gmail.com>
2022-07-26 20:41:28 +0300

    add task_3 solution

---
+++ src/main/java/students/student_Victor_Goloschapov/lesson_6/homework/level_1/Task_3/WhileDefinition3.java
@@ -0,0 +1,14 @@
+package lesson_6.homework.level_1.Task_3;
+
+public class WhileDefinition3 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+/*Решение
+1. Добавил } в конце блока цикла
+*/
