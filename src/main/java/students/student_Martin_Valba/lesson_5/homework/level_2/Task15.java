80d261ad33c66908263a5ba500b8f765c6aba6fa
pecoyn <martinvalba@mail.ru>
2022-07-29 22:29:40 +0300

    st-martin-valba-lesson-5-homework-level-1-2

--- src/main/java/students/student_Martin_Valba/lesson_5/homework/level_2/Task15.java
+++ src/main/java/students/student_Martin_Valba/lesson_5/homework/level_2/Task15.java
@@ -4,6 +4,7 @@ public class Task15 {
     public static void main(String[] args) {
 
         int[] numbers = new int[3];
+
         numbers[0] = (int) (Math.random() * (1 - 2)) + 3;
         numbers[1] = (int) (Math.random() * (4 - 5)) + 6;
         numbers[2] = (int) (Math.random() * (7 - 8)) + 9;
